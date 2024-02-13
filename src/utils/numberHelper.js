0f3ac62828cfefb6478ac9285509b7f18c60b3d7
Lauv <pussaaon@gmail.com>
2024-02-14 00:23:08 +0700

    Adjust currency format consistency

--- src/utils/numberHelper.js
+++ src/utils/numberHelper.js
@@ -2,10 +2,7 @@ export function formatCurrency(value) {
     if (value >= 1000000) {
         return `${(value / 1e6).toFixed(2)}M`;
     } else {
-        return new Intl.NumberFormat("en", {
-            style: "currency",
-            currency: "THB",
-        }).format(value);
+        return formatCommas(value);
     }
 }
 
